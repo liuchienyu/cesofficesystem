2c1a9c65942e2bbc899379a0d922a223fa9a1847
HCC1207 <flyr1207@gmail.com>
2025-08-23 01:45:14 +0800

    0823-2

--- finance.py
+++ finance.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from flask import Flask, render_template, redirect, url_for, render_template, request, session, g
 from datetime import timedelta,date
 
-CONNECTION_STRING ="mongodb+srv://dandy40605:1234@cluster0.qqbqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CONNECTION_STRING ="mongodb+srv://dandy40605:dandy1234@cluster0.qqbqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 client = MongoClient(CONNECTION_STRING,tls=True, tlsAllowInvalidCertificates=True,tz_aware=True )#tls=True, tlsAllowInvalidCertificates=True為解決無法連線問題
 # 設定為 +8 時區
 tz = datetime.timezone(datetime.timedelta(hours=+8))
